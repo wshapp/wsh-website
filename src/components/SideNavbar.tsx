790fe0a2168dd68e13d608683dc94388ca4786b8
Antoine Kingue <etorefr@gmail.com>
2021-04-16 03:56:30 +0200

    ⚡️ improving side navbar

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -1,28 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 import { WshLogo } from "./layout/Icons";
 import Link from "./Link";
 import * as ROUTES from "@constants/routes";
 
-const SideNavbar: React.FC = () => (
-    <Container>
-        <Link href={ROUTES.INDEX}>
-            <Logo />
-        </Link>
-        <Nav>
-            <NavItem>
-                <NavLink href={ROUTES.INDEX}><i className="ri-home-4-line"></i></NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href={ROUTES.MESSAGES}><i className="ri-chat-1-line"></i></NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink href={ROUTES.SETTINGS}><i className="ri-settings-line"></i></NavLink>
-            </NavItem>
-        </Nav>
-    </Container>
-);
+const SideNavbar: React.FC = () => {
+
+    const { pathname } = useRouter();
+
+    return (
+        <Container>
+            <Link href={ROUTES.INDEX}>
+                <Logo />
+            </Link>
+            <Nav>
+                <NavItem>
+                    <NavLink href={ROUTES.INDEX}><i className={pathname === ROUTES.INDEX ? 'ri-home-4-fill' : 'ri-home-4-line'}></i></NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href={ROUTES.MESSAGES}><i className={pathname.includes(ROUTES.MESSAGES) ? 'ri-chat-1-fill' : 'ri-chat-1-line'}></i></NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href={ROUTES.SETTINGS}><i className={pathname.includes(ROUTES.SETTINGS) ? 'ri-settings-fill' : 'ri-settings-line'}></i></NavLink>
+                </NavItem>
+            </Nav>
+        </Container>
+    );
+}
 
 const Container = styled.div`
     display: flex;
@@ -53,6 +59,7 @@ const Nav = styled.ul`
     flex-direction: column;
     margin: auto 0;
     align-items: center;
+    width: 100%;
     
     @media (max-width: 768px) {
         flex-direction: row;
@@ -64,16 +71,8 @@ const Nav = styled.ul`
 const NavLink = styled(Link)``;
 
 const NavItem = styled.li`
-    margin: 15px 0;
+    padding: 15px 0;
     font-size: 20px;
-
-    &:first-child {
-        margin-top: 0;
-    }
-
-    &:last-child {
-        margin-bottom: 0;
-    }
 
     @media (max-width: 768px) {
         margin: 0 15px;
