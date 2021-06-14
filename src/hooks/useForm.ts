5f1d7d7d43b70117568fe524c822ca1ce07b0e7f
Antoine Kingue <etorefr@gmail.com>
2021-06-14 19:03:42 +0200

    🏷️ update useform props

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,21 +1,28 @@
-import React from 'react';
+import React from "react";
 
-export const useForm = (callback, initialState = {}): { onChange: Function, onSubmit: Function, values: any} => {
-    const [values, setValues] = React.useState(initialState);
+export const useForm = (
+  callback,
+  initialState = {}
+): { onChange: (event) => void; onSubmit: (event) => void; values: any } => {
+  const [values, setValues] = React.useState(initialState);
 
-    const onChange = (event) => {
-        if (event.target.getAttribute("contenteditable")) setValues({ ...values, [event.target.getAttribute("name")]: event.target.textContent });
-        else setValues({ ...values, [event.target.name]: event.target.value });
-    };
+  const onChange = (event) => {
+    if (event.target.getAttribute("contenteditable"))
+      setValues({
+        ...values,
+        [event.target.getAttribute("name")]: event.target.textContent,
+      });
+    else setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
-    const onSubmit = (event) => {
-        event.preventDefault();
-        callback();
-    };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    callback();
+  };
 
-    return {
-        onChange,
-        onSubmit,
-        values
-    };
+  return {
+    onChange,
+    onSubmit,
+    values,
+  };
 };
