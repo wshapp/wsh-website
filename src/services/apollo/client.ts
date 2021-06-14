935c66cfe7feff6d097be86b6450b6b851740b54
Antoine Kingue <etorefr@gmail.com>
2021-06-14 19:08:03 +0200

    🔧 update apollo service

--- src/services/apollo/client.ts
+++ src/services/apollo/client.ts
@@ -1,26 +1,33 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { setContext } from 'apollo-link-context';
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import withApollo from "next-with-apollo";
 
 const httpLink = new HttpLink({
-    uri: process.env.REACT_APP_API_URL || 'http://localhost:5000',
-    credentials: 'same-origin',
+  uri: process.env.NEXT_PUBLIC_API || "http://localhost:5000",
+  credentials: "same-origin",
 });
 
 const authLink = setContext(() => {
-    const token = localStorage.getItem('jwtToken');
-    return {
-        headers: {
-            Authorization: token ? `Miskine ${token}` : ''
-        }
-    };
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Miskine ${token}` : "",
+    },
+  };
 });
 
-export const apollo = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache({
-        freezeResults: true
-    }),
-    assumeImmutableResults: true
-})
+export const apollo: any = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default withApollo(
+  ({ ctx, headers, initialState }) =>
+    new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache().restore(initialState || {}),
+    })
+);
