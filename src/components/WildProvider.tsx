dc692a54e73f6d2fb2074edaee9c3e0dabcee7f2
Antoine Kingue <etorefr@gmail.com>
2021-06-14 19:01:33 +0200

    🏷️ update wildprovider props

--- src/components/WildProvider.tsx
+++ src/components/WildProvider.tsx
@@ -1,21 +1,27 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 
-import { AuthContext } from "../context/Auth";
-import * as ROUTES from "../constants/routes";
+import { AuthContext } from "@context/Auth";
+import * as ROUTES from "@constants/routes";
 import Loading from "./Loading";
+import { useRouter } from "next/router";
 
-interface WildProviderProps { noRedirect?: boolean; }
-
-const WildProvider: React.FC<WildProviderProps> = ({ children, noRedirect = false }) => {
-    const { user } = React.useContext(AuthContext);
-    const history = useHistory();
-
-    React.useEffect(() => {
-        if (user && !noRedirect) history.push(ROUTES.HOUSE);
-    }, []);
-
-    return user ? <Loading /> : <>{children}</>;
+interface WildProviderProps {
+  children: React.ReactNode;
+  noRedirect?: boolean;
 }
+
+const WildProvider: React.FC<WildProviderProps> = ({
+  children,
+  noRedirect = false,
+}: WildProviderProps) => {
+  const { user } = React.useContext(AuthContext);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (user && !noRedirect) router.push(ROUTES.HOUSE);
+  }, []);
+
+  return user ? <Loading /> : <>{children}</>;
+};
 
 export default WildProvider;
