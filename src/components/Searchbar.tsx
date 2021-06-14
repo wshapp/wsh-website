bfb11817ee026b5e0060f61bc205f64807f8c9c9
Antoine Kingue <etorefr@gmail.com>
2021-06-14 18:56:26 +0200

    ♻️ refactor searchbar with prettier indentation

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,27 +1,27 @@
 import styled from "styled-components";
 
-const Searchbar = () => (
-    <Container>
-        <Field placeholder="Search..." />
-    </Container>
+const Searchbar: React.FC = () => (
+  <Container>
+    <Field placeholder="Search..." />
+  </Container>
 );
 
 const Container = styled.div`
-    background-color: ${props => props.theme.colors.layout.darker};
-    border-radius: 10px;
-    width: 100%;
+  background-color: ${({ theme }) => theme.colors.layout.darker};
+  border-radius: 10px;
+  width: 100%;
 `;
 
 const Field = styled.input`
-    background-color: transparent;
-    border: none;
-    outline: none;
-    padding: 15px;
-    width: 100%;
-    
-    &::placeholder {
-        color: ${props => props.theme.colors.text.light};
-    }
+  background-color: transparent;
+  border: none;
+  outline: none;
+  padding: 15px;
+  width: 100%;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.text.light};
+  }
 `;
 
 export default Searchbar;
