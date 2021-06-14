f208822cd3832547cd495f9fb1b0a6760b50ed69
Antoine Kingue <etorefr@gmail.com>
2021-06-14 18:54:58 +0200

    🏷️ update safeprovider props

--- src/components/SafeProvider.tsx
+++ src/components/SafeProvider.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { useRouter } from "next/router";
 
-import { AuthContext } from "../context/Auth";
-import * as ROUTES from "../constants/routes";
-import Loading from "../components/Loading";
+import { AuthContext } from "@context/Auth";
+import * as ROUTES from "@constants/routes";
+import Loading from "@components/Loading";
 
-const SafeProvider: React.FC<{ noRedirect?: boolean }> = ({ children, noRedirect = false }) => {
-    const { user } = React.useContext(AuthContext);
-    const history = useHistory();
-
-    React.useEffect(() => {
-        if (!user && !noRedirect) history.push(ROUTES.LANDING)
-    }, []);
-
-    return user ? <>{children}</> : <Loading />;
+interface SafeProviderProps {
+  children: React.ReactNode;
+  noRedirect?: boolean;
 }
+
+const SafeProvider: React.FC<SafeProviderProps> = ({
+  children,
+  noRedirect = false,
+}: SafeProviderProps) => {
+  const { user } = React.useContext(AuthContext);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (!user && !noRedirect) router.push(ROUTES.LANDING);
+  }, [noRedirect, user, router]);
+
+  return user ? <>{children}</> : <Loading />;
+};
 
 export default SafeProvider;
