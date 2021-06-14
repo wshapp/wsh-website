5e7643631b45981d5eb4456b1bf95e6786b361fd
Antoine Kingue <etorefr@gmail.com>
2021-06-14 18:45:01 +0200

    ♻️ refactor inbox with prettier indentation

--- src/components/Inbox.tsx
+++ src/components/Inbox.tsx
@@ -6,52 +6,54 @@ import * as ROUTES from "../constants/routes";
 import Link from "./Link";
 
 const Inbox: React.FC = () => {
-    return (
-        <Container>
-            <Header>
-                <Title href={ROUTES.MESSAGES}>Messages <span>• 50</span></Title>
-            </Header>
-            <MessageList>
-                {Array.from({ length: 50 }, (_, i) => (
-                    <InboxItem id={i} key={i} />
-                ))}
-            </MessageList>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Header>
+        <Title href={ROUTES.MESSAGES}>
+          Messages <span>• 50</span>
+        </Title>
+      </Header>
+      <MessageList>
+        {Array.from({ length: 50 }, (_, i) => (
+          <InboxItem id={i} key={i} />
+        ))}
+      </MessageList>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    background-color: ${props => props.theme.colors.layout.darker};
-    width: 300px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.colors.layout.darker};
+  width: 300px;
 `;
 
 const Header = styled.div`
-    padding: 15px;
+  padding: 15px;
 `;
 
 const Title = styled(Link)`
-    display: flex;
-    align-items: center;
-    font-size: 24px;
-    font-weight: 700;
-    user-select: none;
-    color: ${props => props.theme.colors.text.lightest};
+  display: flex;
+  align-items: center;
+  font-size: 24px;
+  font-weight: 700;
+  user-select: none;
+  color: ${({ theme }) => theme.colors.text.lightest};
 
-    span {
-        margin-left: 5px;
-        font-size: 14px;
-        color: ${props => props.theme.colors.text.light};
-    }
+  span {
+    margin-left: 5px;
+    font-size: 14px;
+    color: ${({ theme }) => theme.colors.text.light};
+  }
 `;
 
 const MessageList = styled.ul`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    width: 100%;
-    overflow: auto;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+  overflow: auto;
 `;
 
 export default Inbox;
