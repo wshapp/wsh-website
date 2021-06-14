1fce69150bb69a82716931f981d6fc4e1c8d3b33
Antoine Kingue <etorefr@gmail.com>
2021-06-14 18:54:07 +0200

    ♻️ refactor postcomments with prettier indentation

--- src/components/PostComments.tsx
+++ src/components/PostComments.tsx
@@ -1,29 +1,29 @@
 import styled from "styled-components";
 
-const PostComments = () => (
-    <Container>
-        <Title>Comments <span>(50)</span></Title>
-        <Content></Content>
-    </Container>
+const PostComments: React.FC = () => (
+  <Container>
+    <Title>
+      Comments <span>(50)</span>
+    </Title>
+    <Content></Content>
+  </Container>
 );
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h3`
-    font-size: 20px;
-    font-weight: 600;
-    user-select: none;
-    span {
-        color: ${props => props.theme.colors.text.light};
-        font-size: 13px;
-    }
+  font-size: 20px;
+  font-weight: 600;
+  user-select: none;
+  span {
+    color: ${({ theme }) => theme.colors.text.light};
+    font-size: 13px;
+  }
 `;
 
-const Content = styled.div`
-    
-`;
+const Content = styled.div``;
 
 export default PostComments;
